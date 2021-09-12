9377ca52f5122306d0c0c1c239ad1a155fcce666
Hoani <hoani.bryson@gmail.com>
2021-09-12 06:07:16 +0100

    Add python move script

---
+++ scripts/move.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import pigpio
+import time
+
+import rospy
+from bittleet.srv import MoveServo, MoveServoRequest, MoveServoResponse
+
+class Move:
+    JOINT_INDEXES = [0, 8, 9, 10, 11, 12, 13, 14, 15]
+
+    def __init__(self):
+        self.pi = pigpio.pi()
+        if not self.pi.connected:
+            return
+        self.handle = self.pi.serial_open("/dev/ttyS0", 115200)
+
+        self.node = rospy.init_node('bittleet_move')
+
+    def __del__(self):
+        if self.handle != None:
+            self.pi.serial_close(self.handle)
+
+    def run(self):
+        if not self.pi.connected:
+            return
+        self.srv = rospy.Service(rospy.get_name() +'/move', MoveServo, self.handle_move)
+        rospy.spin()
+
+    def handle_move(self, req: MoveServoRequest):
+        angle = req.angle
+        if not -90.0 <= angle <= 90.0:
+            raise rospy.ServiceException("angle {} out of range".format(angle))
+        index = req.index
+        if index not in Move.JOINT_INDEXES:
+            raise rospy.ServiceException("index {} invalid".format(index))
+
+        done = False
+        res = MoveServoResponse()
+        while not done:
+            command = f"m {index} {angle:.0f} "
+            print(f"Sending '{command}'")
+            self.pi.serial_write(self.handle, command)
+
+            time.sleep(0.05)
+            len, data = self.pi.serial_read(self.handle, 100)
+
+            if len > 0:
+                try:
+                    res.result = data.decode("utf-8")
+                    done = True
+                except:
+                    pass
+            else:
+                res.result = "<No Reply>"
+                done = True
+
+
+        return res
+
+
+if __name__ == "__main__":
+    try:
+        move = Move()
+        move.run()
+    except rospy.ROSInterruptException:
+        pass
+
