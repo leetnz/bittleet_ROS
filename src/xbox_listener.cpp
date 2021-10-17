4515bf8c0dd630788b500a06470aeb9f4d334d8d
hoani <hoani.bryson@gmail.com>
2021-10-17 18:36:34 +1300

    Make xbox listener which listens for button presses

---
+++ src/xbox_listener.cpp
@@ -0,0 +1,34 @@
+#include <functional>
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <pigpiod_if2.h>
+#include "io/serial_port.h"
+#include "joy/xbox.h"
+
+int main(int argc, char **argv)
+{
+    // SerialPort serial{};
+    // if (serial.ready() == false) {
+    //     return 1;
+    // }
+
+    XboxController controller{};
+    controller.registerButtonPress(
+        XboxController::ButtonEvent{
+            .button = XboxController::Button::X,
+            .callback = []() -> void { 
+                ROS_INFO("X Button Pressed");
+            }
+        }  
+    );
+
+    ros::init(argc, argv, "xbox_listener");
+
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("joy", 10, &XboxController::callback, &controller);
+
+    
+    ros::spin();
+
+    return 0;
+}
