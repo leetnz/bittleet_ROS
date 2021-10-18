2ebfeb32d9a365a2d8dad823b6ecf5ac86b93bfe
hoani <hoani.bryson@gmail.com>
2021-10-18 23:05:34 +1300

    Add quadrant-based movement control to xbox controller

--- src/xbox_listener.cpp
+++ src/xbox_listener.cpp
@@ -8,6 +8,88 @@
 static SerialPort serial;
 static XboxController controller;
 
+class MoveStateMachine {
+    public:
+        MoveStateMachine() = default;
+
+        void xCallback(float value) {
+            xValue = value;
+            QuadrantState current = _quadrant();
+            if (current != lastQuadrant) {
+                lastQuadrant = current;
+                {
+                    std::string cmd = _quadrantMap[current];
+                    ROS_INFO("Writing %s", cmd.c_str());
+                    serial.write(cmd.c_str(), cmd.size());
+                }
+            }
+        }
+
+        void yCallback(float value) {
+            yValue = value;
+
+            QuadrantState current = _quadrant();
+            if (current != lastQuadrant) {
+                lastQuadrant = current;
+                {
+                    std::string cmd = _quadrantMap[current];
+                    ROS_INFO("Writing %s", cmd.c_str());
+                    serial.write(cmd.c_str(), cmd.size());
+                }
+            }
+        }
+
+        static constexpr size_t NumSpeedStates = 5;
+        enum class QuadrantState : int {
+            None = 0,
+            Forward,
+            Left,
+            Backward,
+            Right,
+        };
+        static constexpr size_t NumQuadrantStates = 5;
+
+
+        float xValue = 0.0;
+        float yValue = 0.0;
+        QuadrantState lastQuadrant = QuadrantState::None;
+
+    private:
+        using QuadrantMap = std::map<QuadrantState, std::string>;
+        QuadrantMap _quadrantMap = {
+            {QuadrantState::None,       "kb"},
+            {QuadrantState::Forward,    "kF"},
+            {QuadrantState::Left,       "kL"},
+            {QuadrantState::Right,      "kR"},
+            {QuadrantState::Backward,   "kB"},
+        };
+        
+
+        QuadrantState _quadrant() {
+            const float norm = sqrt(xValue * xValue + yValue * yValue);
+
+            if (norm < 0.2) {
+                return QuadrantState::None;
+            }
+
+            const float theta = atan2(yValue, xValue);
+
+            if (fabs(theta) < M_PI_4) {
+                return QuadrantState::Forward;
+            }
+
+            if (fabs(theta) >  3 * M_PI_4) {
+                return QuadrantState::Backward;
+            }
+
+            if (theta > 0) {
+                return QuadrantState::Left;
+            }
+            
+            return QuadrantState::Right;
+        }
+};
+
 void at_signal(int i) {
    serial.shutdown();
    exit(0);
@@ -39,14 +121,26 @@ int main(int argc, char **argv)
         );
     }
 
-    // controller.registerAnalog(
-    //     XboxController::AnalogEvent{
-    //         .input = XboxController::Analog::LTRIGGER,
-    //         .callback = [](float value) -> void { 
-    //             ROS_INFO("Trigger: %f", value);
-    //         }
-    //     }  
-    // );
+    MoveStateMachine move{};
+    
+    
+    controller.registerAnalog(
+        XboxController::AnalogEvent{
+            .input = XboxController::Analog::LTOGGLE_UD,
+            .callback = [&move](float value) -> void {
+                move.xCallback(value);
+            }
+        }  
+    );
+
+    controller.registerAnalog(
+        XboxController::AnalogEvent{
+            .input = XboxController::Analog::LTOGGLE_LR,
+            .callback = [&move](float value) -> void {
+                move.yCallback(value);
+            }
+        }  
+    );
 
     ros::init(argc, argv, "xbox_listener");
 
